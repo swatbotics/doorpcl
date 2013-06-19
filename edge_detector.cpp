d0e8484238935d4fb967f206f3a2609cd50c22e0
wschnei1 <wschnei1@swarthmore.edu>
2013-06-19 16:26:15 -0400

    camera now displays rgb data

--- edge_detector.cpp
+++ edge_detector.cpp
@@ -13,92 +13,122 @@
 #include <pcl/filters/filter.h>
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/core/core.hpp"
-
-
-boost::shared_ptr<pcl::visualization::PCLVisualizer>
-simpleVis (pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
-{
-  // --------------------------------------------
-  // -----Open 3D viewer and add point cloud-----
-  // --------------------------------------------
-  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
-  viewer->setBackgroundColor (0, 0, 0);
-  viewer->addPointCloud<pcl::PointXYZ> (cloud, "sample cloud");
-  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud");
-  //viewer->addCoordinateSystem (1.0);
-  viewer->initCameraParameters ();
-  return (viewer);
-}
+#include <pcl/common/common_headers.h>
+#include <pcl/features/normal_3d.h>
+#include "opencv2/highgui/highgui.hpp"
 
 class SimpleOpenNIViewer
 {
   public:
+    typedef pcl::PointXYZRGBA Point;  
+    typedef pcl::PointCloud<Point> PointCloud;
+    typedef std::vector< cv::Vec4i > LineArray;
+    typedef pcl::visualization::PCLVisualizer Display;
+    typedef pcl::visualization::PointCloudColorHandlerRGBField<Point> 
+                ColorHandler;
 
+    //pcl::visualization::CloudViewer viewer;
+    //SimpleOpenNIViewer () : viewer ("PCL OpenNI Viewer") {}
 
-    SimpleOpenNIViewer () : viewer ("PCL OpenNI Viewer") {}
-    void cloud_cb_ (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud)
-    {
-    
-      if (!viewer.wasStopped()){
-	//viewer.showCloud( cloud );
-      }
-      pcl::PointCloud<pcl::PointXYZ>::Ptr newCloud ( segment( cloud ) );
-      if ( newCloud == 0 ){
-	return;
-      }
-      viewer.showCloud( newCloud );
+    PointCloud::Ptr inlierCloud;
+    Display * viewer;
+
+    SimpleOpenNIViewer (){
+
+        PointCloud::Ptr temp(new PointCloud );
+        inlierCloud = temp;
+
+        viewer = new Display;
+        cv::namedWindow( "Matrices", CV_WINDOW_AUTOSIZE );
+
+        //viewer->setBackgroundColor (0,0,0);
+        ColorHandler rgb(inlierCloud);
+        pcl::visualization::PointCloudColorHandlerCustom<Point>
+            inlierColor( inlierCloud, 1,0, 0 );
+
+        viewer->addPointCloud<Point> (inlierCloud, inlierColor, "cloud");
+        viewer->setPointCloudRenderingProperties 
+           (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "cloud");
+
+        viewer->addPointCloud<Point> (inlierCloud, 
+                                      inlierColor, 
+                                      "inlierCloud");
+        viewer->setPointCloudRenderingProperties 
+           (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "InlierCloud");
+
+        viewer->addCoordinateSystem (1.0);
+        viewer->initCameraParameters();
+        /*
+        viewer->setCameraParameters (0.0781179,12.9301/-0.0229071,
+                                      -0.133992,-0.00206487/-0.213767,
+                                      0.0172128,-3.13103/-0.0151043,
+                                      -0.998764,-0.0473431/0.523599/683,
+                                      384/54,52);
+        */
     }
+
+    //create a viewer that holds lines and a point cloud.
+    void updateViewer( const PointCloud::ConstPtr &cloud,
+                           const  LineArray & lines ){
+        viewer->updatePointCloud( inlierCloud, "inlierCloud");
+        viewer->updatePointCloud( cloud, "cloud");
+
+        for( int i = 0; i < lines.size(); i ++ ){
+            const cv::Vec4i line = lines[i];
+            int index1, index2;
+            index1 = cloud->width * line[0] + line[1];
+            index2 = cloud->width * line[2] + line[3];
+            viewer->addLine<Point> (cloud->points[index1],
+                                    cloud->points[index2],
+                                    "line"                  );
+        }
+    }
+        
+    void cloud_cb_ (const PointCloud::ConstPtr &cloud)
+    {
+      if ( !viewer->wasStopped() ){
+        LineArray lines;
+        segment( cloud, lines );
+        updateViewer( cloud, lines );
+        viewer->spinOnce (100);
+      }
+
+      cout << "ended Call back\n";
+    }
+
+
     void run ()
     {
+
       pcl::Grabber* interface = new pcl::OpenNIGrabber();
-      boost::function<void (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr&)> f =
+      boost::function<
+          void (const PointCloud::ConstPtr&)> f =
           boost::bind (&SimpleOpenNIViewer::cloud_cb_, this, _1);
       interface->registerCallback (f);
       interface->start ();
-      while (!viewer.wasStopped())
+
+      while (!viewer->wasStopped())
       {
         boost::this_thread::sleep (boost::posix_time::seconds (1));
       }
-      interface->stop ();
+      
+      interface->stop();
     }
-    pcl::visualization::CloudViewer viewer;
 
-    void simpleDisplay( pcl::PointCloud<pcl::PointXYZ>::Ptr cloud )
-    {
-      boost::shared_ptr<pcl::visualization::PCLVisualizer> view;
-      view = simpleVis(cloud);
-      while (!view->wasStopped ())
-      {
-        view->spinOnce (100);
-        boost::this_thread::sleep (boost::posix_time::microseconds (100000));
-      }
-    }
   private:
 
     
-    pcl::PointCloud<pcl::PointXYZ>::Ptr segment (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud)
+    void segment(const PointCloud::ConstPtr &cloud, 
+                                        LineArray& lines)
     {
-      pcl::PointCloud<pcl::PointXYZ>::Ptr culledCloud (new pcl::PointCloud<pcl::PointXYZ>);
-      pcl::PointCloud<pcl::PointXYZ>::Ptr inlierCloud ( new pcl::PointCloud<pcl::PointXYZ> );
+      PointCloud::Ptr
+             culledCloud (new PointCloud);
+
       pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
       pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-      /*
-      cout << "Height: " << cloud->height << "\tWidth: " << cloud->width << endl;
-      std::vector<int> index;
-      
-      pcl::removeNaNFromPointCloud(*cloud, *culledCloud, index);
-      cout << "Height: " << culledCloud->height << "\tWidth: " << culledCloud->width << endl;
 
-	// the model needs at least 3 data points to get a plane model, 
-	//else it will segfault.
-      if ( index.size() <= 3 ){
-	cout << "Invalid cloud data" << endl;
-	pcl::PointCloud<pcl::PointXYZ>::Ptr nullCloud;
-        return nullCloud;
-      }
-      */
       // Create the segmentation object
-      pcl::SACSegmentation<pcl::PointXYZ> seg;
+      pcl::SACSegmentation<Point> seg;
       // Optional
       seg.setOptimizeCoefficients (false);
       // Mandatory
@@ -111,40 +141,62 @@ class SimpleOpenNIViewer
       
       if (inliers->indices.size () == 0)
       {
-        PCL_ERROR ("Could not estimate a planar model for the given dataset.");
+        PCL_ERROR("Could not estimate a planar model for the given data.");
         cout << "Invalid cloud data" << endl;
-	pcl::PointCloud<pcl::PointXYZ>::Ptr nullCloud;
-        return nullCloud;
-        //exit(-1);
+        return;
       }
-      for ( int i = 0; i < inliers->indices.size() ; i ++ ){
-        cout << "\n" << inliers->indices[i];
-      }
-      pcl::copyPointCloud<pcl::PointXYZ>(*cloud, inliers->indices, *inlierCloud);
-      cv::Mat lines =
-	 cv::Mat::zeros(cloud->height, cloud->width, cv::DataType<int>::type );
-       
-      //simpleDisplay(inlierCloud);
-      return inlierCloud;
+      pcl::copyPointCloud<Point>(*cloud, 
+                                         inliers->indices,
+                                         *inlierCloud);
+      cv::Mat majorPlane = cv::Mat::zeros(cloud->height * cloud->width,
+                                     1 , CV_8UC1 );
+      cloudToMat(inliers, majorPlane );
+      
+      //reshape the matrix to the shape of the point cloud.
+      majorPlane.reshape( cloud->height, cloud->width );
+
+      findLines( majorPlane, lines );  
     }
+
     void cloudToMat(const pcl::PointIndices::Ptr & validPoints, cv::Mat &mat)
     {
+      //set the values of mat that correspond to being on the major plane of interest.
+      //These values should be 1, we will end up with a binary matrix: a value of 1 is on the plane,
+      //a value of 0 is off the plane.
       for (int i=0; i < validPoints->indices.size(); i++){
-	  int value1, value2;
-	  if (true){
-            mat.at<int>( value1, value2 ) = 1;
+	    int index = validPoints->indices[i];
+        mat.at<int>( index, 1 ) = 255;
 	  }
-      }
+      
     }
     
-    void findLines(const cv::Mat & src, std::vector<cv::Vec4i> lines)
+    void findLines(const cv::Mat & src, LineArray lines)
     {
- 	cv::Mat dst, cdst;
- 	cv::Canny(src, dst, 50, 200, 3);
- 	cv::cvtColor(dst, cdst, CV_GRAY2BGR);
 
-  	cv::HoughLinesP(dst, lines, 1, CV_PI/180, 50, 50, 10 );
-	
+        cv::Mat dst, cdst, temp;
+        /*
+        temp = src;
+        temp.reshape( 640, 480 );
+        cv::imshow( "Matrices", src );
+        while ( cv::waitKey( 0 ) < 1 ){
+            boost::this_thread::sleep (boost::posix_time::seconds (1));
+        }
+        */
+        try{
+            cv::blur( src, dst, cv::Size(3,3) );
+            cv::Canny(dst, dst, 50, 200, 3);
+        }
+
+        catch ( exception & e ){
+            cout << e.what();
+            return;
+        }
+
+       
+        cv::cvtColor(dst, cdst, CV_GRAY2BGR);
+
+        cv::HoughLinesP(dst, lines, 1, CV_PI/180, 50, 50, 10 );
+        
     }
 };
 
